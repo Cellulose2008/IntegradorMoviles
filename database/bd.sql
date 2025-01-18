4baa9ebdd15eaeb778b281f774dcce0828a46dac
Cellulose2008 <celiasofiamagos2008@gmail.com>
2025-01-18 13:19:58 -0600

    Add files via upload

---
+++ database/bd.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 18, 2025 at 08:19 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cuenta`
+--
+
+CREATE TABLE `cuenta` (
+  `id` smallint(6) NOT NULL,
+  `user` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cuenta`
+--
+
+INSERT INTO `cuenta` (`id`, `user`, `password`) VALUES
+(1, 'celia', 'password'),
+(2, 'valentino', 'password'),
+(3, 'gusa', 'password');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cuenta`
+--
+ALTER TABLE `cuenta`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

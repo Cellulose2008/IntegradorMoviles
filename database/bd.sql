3bab8162d96d4e72a78bea3309098ff515895df0
Pejecal <161546937+Pejecal@users.noreply.github.com>
2025-01-19 18:53:07 -0600

    V2

--- database/bd.sql
+++ database/bd.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 18, 2025 at 08:19 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-01-2025 a las 01:52:04
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bd`
+-- Base de datos: `bd`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cuenta`
+-- Estructura de tabla para la tabla `cuenta`
 --
 
 CREATE TABLE `cuenta` (
@@ -34,7 +34,7 @@ CREATE TABLE `cuenta` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `cuenta`
+-- Volcado de datos para la tabla `cuenta`
 --
 
 INSERT INTO `cuenta` (`id`, `user`, `password`) VALUES
@@ -42,15 +42,33 @@ INSERT INTO `cuenta` (`id`, `user`, `password`) VALUES
 (2, 'valentino', 'password'),
 (3, 'gusa', 'password');
 
+-- --------------------------------------------------------
+
 --
--- Indexes for dumped tables
+-- Estructura de tabla para la tabla `recordatorios`
+--
+
+CREATE TABLE `recordatorios` (
+  `Titulo` varchar(30) NOT NULL,
+  `Desarrollo` varchar(300) DEFAULT NULL,
+  `Fecha` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `cuenta`
+-- Indices de la tabla `cuenta`
 --
 ALTER TABLE `cuenta`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `recordatorios`
+--
+ALTER TABLE `recordatorios`
+  ADD PRIMARY KEY (`Titulo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
